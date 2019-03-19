ee6e3141c98dea375bfcb6b9962cb87cc5e1ce37
victor <malai9696@gmail.com>
2019-03-19 13:49:14 +0200

    Add loan CRUD

---
+++ database/migrations/2019_03_19_093040_create_loans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('book_id')->unsigned();
+			$table->integer('status')->comment('0: Pending, 1: Approved, 2: Restored');
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('book_id')->references('id')->on('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
